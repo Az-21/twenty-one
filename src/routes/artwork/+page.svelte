f56a99f05af6887b4c803347e8832952f988b2ed
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-10-29 23:58:12 +0530

    🆕 added artwork page template

---
+++ src/routes/artwork/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Navbar from '../../components/Navbar.svelte';
+  import { kSpacing } from '@static/Resource.svelte';
+</script>
+
+<Navbar />
+<div class="{kSpacing} grid grid-cols-2 gap-4">
+  <div class="bg-secondaryContainer w-full h-96 rounded-xl"></div>
+</div>
