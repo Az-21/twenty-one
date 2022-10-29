1599552ba850556b8d7dc39db6ef2bde4a5124a9
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-10-29 23:57:54 +0530

    🍫 added navbar component

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { kSpacing } from '@static/Resource.svelte';
+</script>
+
+<div class="border-b-2">
+  <div class="{kSpacing} place-content-between flex place-items-baseline">
+    <!-- Left -->
+    <div class="flex gap-12 place-items-baseline">
+      <p class="text-4xl font-bold">Twenty One</p>
+      <div class="text-primary hover:opacity-100 opacity-60 text-xl flex gap-4">
+        <a href="/artwork">Artwork</a>
+      </div>
+    </div>
+
+    <!-- Right -->
+    <div class="text-tertiary opacity-60 hover:opacity-100 text-xl flex gap-4">
+      <a href="https://github.com/Az-21/twenty-one">GitHub</a>
+    </div>
+  </div>
+</div>
