cbdd3c95c9b52b375a7922037467898aff42de4e
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-11-02 22:36:35 +0530

    🚀 added material 3 button styles

--- src/static/Resource.svelte
+++ src/static/Resource.svelte
@@ -1,3 +1,5 @@
 <script lang="ts" context="module">
   export const width: string = 'mx-auto w-11/12 py-4';
+  export const buttonActive: string = 'rounded-full bg-primary text-onPrimary';
+  export const buttonPassive: string = 'rounded-full border-2 border-outline';
 </script>
