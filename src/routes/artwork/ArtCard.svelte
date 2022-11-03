65f3e7042e18d6182508f678c7aabd095ee1aa95
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-11-03 13:01:44 +0530

    ⬇️ reduced icon size and inner padding

--- src/routes/artwork/ArtCard.svelte
+++ src/routes/artwork/ArtCard.svelte
@@ -29,12 +29,12 @@
 
   <!-- Actions -->
   <div class="p-4 mt-8 flex justify-end gap-4">
-    <a href="{mirrorLink}" class="px-8 py-3 flex {buttonPassive} items-center">
-      <i class="fa-regular fa-images fa-xl pr-4"></i>
+    <a href="{mirrorLink}" class="px-8 py-2 flex {buttonPassive} items-center">
+      <i class="fa-regular fa-images fa-lg pr-4"></i>
       <p class="text-lg">Gallery</p>
     </a>
-    <a href="{redditLink}" class="px-8 py-3 flex {buttonActive} items-center">
-      <i class="fa-brands fa-reddit fa-xl pr-4"></i>
+    <a href="{redditLink}" class="px-8 py-2 flex {buttonActive} items-center">
+      <i class="fa-brands fa-reddit fa-lg pr-4"></i>
       <p class="text-lg">Album</p>
     </a>
   </div>
